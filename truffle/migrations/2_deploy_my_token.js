94cd983af81f27a8db9a2ec03d1a329a58d452d4
HOLO-png <69238651+HOLO-png@users.noreply.github.com>
2022-09-23 11:57:15 +0700

    buoi3

--- truffle/migrations/2_deploy_my_token.js
+++ truffle/migrations/2_deploy_my_token.js
@@ -1,12 +1,13 @@
 const MyToken = artifacts.require("MyToken");
 const MyTokenSale = artifacts.require("MyTokenSale");
+require('dotenv').config({path: '../.env'})
 
 module.exports = async function (deployer) {
   let addr = await web3.eth.getAccounts()
 
-  await deployer.deploy(MyToken, 1000000000);
+  await deployer.deploy(MyToken, process.env.INITIAL_TOKENS);
   await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address);
 
   let instance = await MyToken.deployed()
-  await instance.transfer(MyTokenSale.address, 1000000)
+  await instance.transfer(MyTokenSale.address, process.env.INITIAL_TOKENS)
 };
